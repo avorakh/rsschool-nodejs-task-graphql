7fdd1a7edce91a18a97ca77b6da31d6f04e3a307
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-11-04 00:36:04 +0100

    feat: add complexity limit for queries

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,10 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 import { graphql, parse, validate } from 'graphql';
+import depthLimit  from 'graphql-depth-limit';
 
+
+const MAX_DEPTH: number = 5;
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -18,7 +21,7 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       const { query, variables } = req.body;
 
-      const errors = validate(schema, parse(query));
+      const errors = validate(schema, parse(query), [depthLimit(MAX_DEPTH)]);
       
       if (errors.length > 0) {
         return { errors };
